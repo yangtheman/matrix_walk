16204cf229efa8143da7a052d607c62f76e9fd91
Yang Chung <yangtheman@gmail.com>
2014-04-07 10:19:55 -0700

    Add position_valid? method

--- lib/matrix_walk.rb
+++ lib/matrix_walk.rb
@@ -1,9 +1,24 @@
 class MatrixWalk
 
-  attr_reader :board
+  DIRECTIONS = [:west, :south, :north, :east]
+  attr_reader :board, :steps
 
   def initialize(n, m)
     @board = Array.new(n) { Array.new(m) {|m| 0 }}
+    @board[0][0] = 1
+    @steps = 1
+    @current_position = [0, 0]
+    @current_direction = 0
+  end
+
+  def move_to_next
+  end
+
+  def next_position
+  end
+
+  def position_valid?(coord)
+    @board[coord[0], coord[1]] == 0
   end
 
 end
--- spec/matrix_walk_spec.rb
+++ spec/matrix_walk_spec.rb
@@ -4,7 +4,7 @@ require 'rspec'
 describe MatrixWalk do
 
   describe ".new" do
-    it "should create NxM matrix" do
+    it "should create NxN matrix" do
       matrix = MatrixWalk.new(2, 2)
       matrix.board.size.should == 2
       matrix.board.first.size.should == 2
@@ -16,6 +16,18 @@ describe MatrixWalk do
       matrix.board.first.size.should == 10
     end
 
+    it "should initialize steps with 0" do
+      matrix = MatrixWalk.new(2, 2)
+      matrix.steps.should == 1
+    end
+  end
+
+  describe "#position_valid?" do
+    it "should see if position is valid given an coordinate array" do
+      matrix = MatrixWalk.new(2, 2)
+      matrix.position_valid?([-1, -1]).should be_false
+      matrix.position_valid?([0, 0]).should be_false
+    end
   end
 
 end
