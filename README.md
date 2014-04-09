076c8699090a2c7e91ef376cabf7aed45547a259
Yang Chung <yangtheman@gmail.com>
2014-04-09 13:44:50 -0700

    Completed, but NEED MORE TESTS!

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-matrix_walk
+Matrix Walk
 ===========
+
+Take one step at a time, and turn clockwise, and move if it's within the board and have not been visited before.
+
+What's the number of steps taken when there is nowhere to move.
+
+```sh
+1.9.3-p194 :001 > load 'lib/matrix_walk.rb'
+ => true
+1.9.3-p194 :002 > matrix = MatrixWalk.new(3, 3)
+ => #<MatrixWalk:0x007fd36d493570 @board=[[1, 0, 0], [0, 0, 0], [0, 0, 0]], @steps=1, @current_position=[0, 0], @current_direction=0>
+1.9.3-p194 :003 > matrix.take_walk
+
+Number of steps: 9
+1 2 9
+4 3 8
+5 6 7
+
+ => nil
+1.9.3-p194 :004 > matrix = MatrixWalk.new(7, 4)
+ => #<MatrixWalk:0x007fd36d4a26b0 @board=[[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], @steps=1, @current_position=[0, 0], @current_direction=0>
+1.9.3-p194 :005 > matrix.take_walk
+
+Number of steps: 18
+1 2 0 0
+4 3 0 0
+5 6 0 0
+8 7 0 0
+9 10 0 0
+12 11 16 17
+13 14 15 18
+
+ => nil
+1.9.3-p194 :006 > matrix = MatrixWalk.new(9, 9)
+ => #<MatrixWalk:0x007fd36d4c09d0 @board=[[1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]], @steps=1, @current_position=[0, 0], @current_direction=0>
+1.9.3-p194 :007 > matrix.take_walk
+
+Number of steps: 81
+1 2 55 54 51 50 47 46 45
+4 3 56 53 52 49 48 43 44
+5 6 57 58 79 78 77 42 41
+8 7 60 59 80 75 76 39 40
+9 10 61 62 81 74 73 38 37
+12 11 64 63 68 69 72 35 36
+13 14 65 66 67 70 71 34 33
+16 15 20 21 24 25 28 29 32
+17 18 19 22 23 26 27 30 31
+
+ => nil
+```
--- lib/matrix_walk.rb
+++ lib/matrix_walk.rb
@@ -1,6 +1,6 @@
 class MatrixWalk
 
-  DIRECTIONS = [:west, :south, :north, :east]
+  DIRECTIONS = [:east, :south, :west, :north]
   attr_reader :board, :steps
 
   def initialize(n, m)
@@ -11,33 +11,51 @@ class MatrixWalk
     @current_direction = 0
   end
 
-  def move_to_next
-    coord = next_position
-    if coord
+  def take_walk
+    while coord = next_position
       @steps += 1
       @current_position = [coord[0], coord[1]]
       @board[coord[0]][coord[1]] = @steps
-    else
-      puts "Number of steps: #{@steps}"
-      exit
     end
+
+    print_board
+  end
+
+  def print_board
+    puts "\nNumber of steps: #{@steps}"
+    @board.each do |row|
+      row.each do |col|
+        print "#{col} "
+      end
+      print "\n"
+    end
+    print "\n"
+  end
+
+  private
+
+  def position_valid?(coord)
+    coord[0] >= 0 && coord[1] >= 0 && @board[coord[0]] && @board[coord[0]][coord[1]] == 0
   end
 
   def next_position
+    0.upto(DIRECTIONS.size-1) do |i|
+      next_position = get_next_position(DIRECTIONS[@current_direction])
+      @current_direction = (@current_direction + 1) % 4
+      return next_position if position_valid?(next_position)
+    end
+    return nil
   end
 
   def get_next_position(direction)
-    next_position = @current_position
-    next_position = case direction
-      when :west then next_position[0] += 1
-      when :south then next_position[1] += 1
-      when :north then next_position[1] -= 1
-      when :east then next_position[0] -= 1
+    next_position = @current_position.clone
+    case direction
+    when :east then next_position[1] += 1
+    when :south then next_position[0] += 1
+    when :west then next_position[1] -= 1
+    when :north then next_position[0] -= 1
     end
-  end
-
-  def position_valid?(coord)
-    @board[coord[0], coord[1]] == 0
+    next_position
   end
 
 end
